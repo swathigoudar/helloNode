c23d0c80c275608d9f9df938dd78492109c54353
andalike <43137695+andalike@users.noreply.github.com>
2019-01-05 15:19:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 4200
 
-app.get('/', (req, res) => res.send('Hello Worls, This is toe check the updated code!'))
+app.get('/', (req, res) => res.send('Hello World, This is toe check the updated code!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
