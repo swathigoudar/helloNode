6009e4cecbb6984573afb5d85251ba1ae29914c9
andalike <43137695+andalike@users.noreply.github.com>
2019-03-31 12:28:03 +0530

    Prediction for Match

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 4200
 
-app.get('/', (req, res) => res.send('Hello World, This is toe check the updated code!'))
+app.get('/', (req, res) => res.send('RCb vs SRH. RCB: 268/2 , SRH: 128/10'))
 
-app.listen(port, () => console.log(`Hello, Good Morning From Tecmax!  ${port}!`))
+app.listen(port, () => console.log(`No, No We are not 420!  ${port}!`))
