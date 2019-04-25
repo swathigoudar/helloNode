842a3e102f80c44a5c4ba3bfcf69e3c00dfa7857
andalike <43137695+andalike@users.noreply.github.com>
2019-04-25 09:17:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 4200
 
-app.get('/', (req, res) => res.send('RCB vs SRH. RCB: 268/2 , SRH: 128/10, Ee Sala Cup Namde!! And  What do we do after each match? https://s3.ap-southeast-1.amazonaws.com/images.asianage.com/images/aa-Cover-qoacubcg7ht53umeo8npdsvbc1-20180827232157.Medi.jpeg '))
+app.get('/', (req, res) => res.send('Hello There!!'))
 
 app.listen(port, () => console.log(`No, No We are not 420!  ${port}!`))
