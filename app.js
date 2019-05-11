8f31ed2d91a119641b919407abf70f3a9f8825f1
andalike <43137695+andalike@users.noreply.github.com>
2019-05-11 09:19:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 4200
 
-app.get('/', (req, res) => res.send('Hello There!!'))
+app.get('/', (req, res) => res.send('Munde Salaa Cup Namde!!'))
 
 app.listen(port, () => console.log(`No, No We are not 420!  ${port}!`))
