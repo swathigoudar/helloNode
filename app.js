a9e7889c3f028818c0e58c010ce6247d4460be6f
Ubuntu <ubuntu@ip-172-31-36-25.us-east-2.compute.internal>
2019-06-21 15:48:29 +0000

    sample test

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 4200
 
-app.get('/', (req, res) => res.send('Ee Salaa Cup Namde - MI!!'))
+app.get('/', (req, res) => res.send('Changing Default Message'))
 
 app.listen(port, () => console.log(`No, No We are not 420!  ${port}!`))
