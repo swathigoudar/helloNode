484f9fb7c3250b1ddb7b180f11bb8fcea05eaa27
andalike <43137695+andalike@users.noreply.github.com>
2019-08-23 23:21:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 4200
 
-app.get('/', (req, res) => res.send('Testing-2'))
+app.get('/', (req, res) => res.send('Hello Testing the Get Request'))
 
 app.listen(port, () => console.log(`This will be the console message!  ${port}!`))
