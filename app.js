af1b8320b04d3abc1297a4b326b4aff01e5e0669
swathigoudar <swathigoudar@gmail.com>
2019-12-08 11:47:53 +0530

    Update app.js
    modified one

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 4201
 
-app.get('/', (req, res) => res.send('See This? Means It works, Now see this :hsahckjjasd/'))
+app.get('/', (req, res) => res.send('See This? Means It works, Now see this :prime number/'))
 
 app.listen(port, () => console.log(`This will be the console message!  ${port}!`))
